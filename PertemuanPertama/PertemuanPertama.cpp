f6fd496adaa2041ffdb9d64d74b617752a600a71
rifqy <rifqy@MSI>
2024-02-26 13:00:07 +0800

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //begin
+       // numeric njejari, nLuas
+       // display 'Masukan jejari= '
+       // accept nJejari
+       // compute nLuas = 3.14 * r * r
+    //end
+
+    double nJejari, nLuas;
+    cout << "Masukan Jejari = ";
+    cin >> nJejari;
+    nLuas = 3.14 * nJejari * nJejari;
+    cout << "luasnya = " << nLuas << endl;
+}
+
+
